25772b93e0edd977c6c35f6e6e3392f54ef38097
DylanCairns2303 <dylacasismusic@gmail.com>
2024-08-14 23:55:33 +0200

    Proc Anim Secured

--- Assets/Scripts/PRA/procAnimIKFootSolver.cs
+++ Assets/Scripts/PRA/procAnimIKFootSolver.cs
@@ -6,16 +6,15 @@ public class procAnimIKFootSolver : MonoBehaviour
 {
     [SerializeField] private LayerMask terrainLayer = default;
     [SerializeField] private Transform body = default;
+    [SerializeField] private procAnimIKFootSolver[] otherFoot = default;
     [SerializeField] private float speed = 1;
     [SerializeField] private float stepDistance = 4;
     [SerializeField] private float stepLength = 4;
     [SerializeField] private float stepHeight = 1;
     [SerializeField] private Vector3 footOffset = default;
 
-    private Vector3 targetLocation;
-
-    //Vector3 footSpacing;
-    float footSpacing;
+    //float footSpacing;
+    Vector3 footSpacing;
     Vector3 oldPos, currentPos, newPos;
     Vector3 oldNormal, currentNormal, newNormal;
     float lerp;
@@ -23,47 +22,54 @@ public class procAnimIKFootSolver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //footSpacing = transform.localPosition;
-        footSpacing = transform.localPosition.x;
+        //footSpacing = transform.localPosition.x;
+        footSpacing = transform.localPosition;
         currentPos = newPos = oldPos = transform.position;
         currentNormal = newNormal = oldNormal = transform.up;
         lerp = 1;
-
-        targetLocation = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(footSpacing);
         transform.position = currentPos;
         transform.up = currentNormal;
 
         //Ray ray = new Ray(body.position + (body.right * footSpacing), Vector3.down);
-        //Ray ray = new Ray(transform.position, Vector3.down);
-        Ray ray = new Ray(targetLocation, Vector3.down);
-
-        //if(Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer.value))
-        //{
-        //    if(Vector3.Distance(newPos, info.point) > stepDistance && !otherFoot.Ismoving() && lerp >= 1)
-        //    {
-        //        lerp = 0;
-        //        int direction = body.InverseTransformPoint(info.point).z > body.InverseTransformPoint(newPos).z ? 1 : -1;
-        //        newPos = info.point + (body.forward * stepLength * direction) + footOffset;
-        //        newNormal = info.normal;
-        //    }
-        //}
+        Ray ray = new Ray(body.position + footSpacing, Vector3.down);
 
         if (Physics.Raycast(ray, out RaycastHit info, 10, terrainLayer.value))
         {
-            //Debug.Log("Hit");
-            if (Vector3.Distance(newPos, info.point) > stepDistance && lerp >= 1)
+            for (int i = 0; i <= 6; i++)
             {
-                lerp = 0;
-                int direction = body.InverseTransformPoint(info.point).z > body.InverseTransformPoint(newPos).z ? 1 : -1;
-                newPos = info.point + (body.forward * stepLength * direction) + footOffset;
-                newNormal = info.normal;
+                if (Vector3.Distance(newPos, info.point) > stepDistance && !otherFoot[i].IsMoving() && lerp >= 1)
+                {
+                    lerp = 0;
+                    int direction = body.InverseTransformPoint(info.point).z > body.InverseTransformPoint(newPos).z ? 1 : -1;
+                    newPos = info.point + (body.forward * stepLength * direction) + footOffset;
+                    newNormal = info.normal;
+                }
             }
+            //if (Vector3.Distance(newPos, info.point) > stepDistance && !otherFoot.IsMoving() && lerp >= 1)
+            //{
+            //    lerp = 0;
+            //    int direction = body.InverseTransformPoint(info.point).z > body.InverseTransformPoint(newPos).z ? 1 : -1;
+            //    newPos = info.point + (body.forward * stepLength * direction) + footOffset;
+            //    newNormal = info.normal;
+            //}
+
+            //stepDistance += Random.Range(-0.1f, 0.1f);
+
+            //if (Vector3.Distance(newPos, info.point) > stepDistance && lerp >= 1)
+            //{
+            //    //stepLength += Random.Range(-0.2f, 0.2f);
+
+            //    lerp = 0;
+            //    int direction = body.InverseTransformPoint(info.point).z > body.InverseTransformPoint(newPos).z ? 1 : -1;
+            //    newPos = info.point + (body.forward * stepLength * direction) + footOffset;
+            //    newNormal = info.normal;
+            //    //Debug.Log(newNormal);
+            //}
         }
 
         if (lerp < 1)
@@ -71,8 +77,11 @@ public class procAnimIKFootSolver : MonoBehaviour
             Vector3 tempPos = Vector3.Lerp(oldPos, newPos, lerp);
             tempPos.y += Mathf.Sin(lerp * Mathf.PI) * stepHeight;
 
+            Debug.Log(lerp);
+
             currentPos = tempPos;
             currentNormal = Vector3.Lerp(oldNormal, newNormal, lerp);
+
             lerp += Time.deltaTime * speed;
         }
         else
@@ -85,7 +94,7 @@ public class procAnimIKFootSolver : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(newPos, 0.5f);
+        Gizmos.DrawSphere(newPos, 0.2f);
     }
 
     public bool IsMoving()
