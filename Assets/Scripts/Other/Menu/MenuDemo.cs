9d67f1bcd216c49ff9b0d1287933c94835518ca0
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-08-29 16:03:04 +0200

    Demo Scenes

---
+++ Assets/Scripts/Other/Menu/MenuDemo.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuDemo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            OnDemoGameClick();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            OnDemoMapClick();
+        }
+    }
+
+    public void OnDemoGameClick()
+    {
+        SceneManager.LoadScene("DemoSceneGameplay");
+    }
+
+    public void OnDemoMapClick()
+    {
+        SceneManager.LoadScene("DemoSceneMap");
+    }
+}
