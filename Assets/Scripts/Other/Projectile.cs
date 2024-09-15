ee51ef9de3c134ae27edda901879c73e3a71df35
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-09-15 20:32:30 +0200

    Added ammoCount

--- Assets/Scripts/Other/Projectile.cs
+++ Assets/Scripts/Other/Projectile.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Projectile : MonoBehaviour
 {
@@ -13,10 +14,17 @@ public class Projectile : MonoBehaviour
     public Transform projectileSpawnPoint;
     public float Speed;
 
+    [Space(5)]
+    public int ammoCount;
+    public int ammoReloadTimeInSec = 1;
+    private int framecounter;
+    public Text ammoText;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        ammoCount = 8;
+        framecounter = 0;
     }
 
     // Update is called once per frame
@@ -27,9 +35,18 @@ public class Projectile : MonoBehaviour
             ShootAtPlayer();
         }*/
 
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        Ammo();
+
+        if(Input.GetKeyDown(KeyCode.Mouse0) && ammoCount > 0)
         {
             Shoot();
+            
+        }
+        else
+        {
+            ///sound effect to show you're out of ammo
+            ///
+
         }
 
     }
@@ -46,5 +63,28 @@ public class Projectile : MonoBehaviour
         Rigidbody bulletRB = bullet.GetComponent<Rigidbody>();
         bulletRB.AddForce(bulletRB.transform.forward * Speed);
         Destroy(bullet, 5f);
+
+        ammoCount--;
+
+        //Invoke(nameof(Reload), 2);
+    }
+
+
+    void Ammo()
+    {
+        framecounter++;
+
+        if(framecounter >= 60*ammoReloadTimeInSec && ammoCount < 8)
+        {
+            ammoCount++;
+            framecounter = 0;
+        }
+
+        ammoText.text = ammoCount.ToString();
+    }
+
+    void Reload()
+    {
+        ammoCount++;
     }
 }
