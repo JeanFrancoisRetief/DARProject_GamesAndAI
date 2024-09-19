f54c5e2857786b1c85292d458729dfd1fc3dc23e
DylanCairns2303 <dylacasismusic@gmail.com>
2024-09-19 20:22:44 +0200

    Roach Added

---
+++ Assets/Scripts/Other/EnemyAnimScripts/RoachAnims.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoachAnims : MonoBehaviour
+{
+    public EnemyMovement enemyScript;
+    public Animator roachAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyScript.distance > 20)
+        {
+            roachAnimator.SetBool("InRange", false);
+        }
+        else
+        {
+            roachAnimator.SetBool("InRange", true);
+        }
+        
+        if(enemyScript.distance < 20 && enemyScript.distance > 5)
+        {
+            roachAnimator.SetBool("AttackRange", false);
+        }
+        else
+        {
+            roachAnimator.SetBool("AttackRange", true);
+        }
+    }
+}
