e52b4c57c2a29d2ed122549e50d68ad353fa9916
DylanCairns2303 <dylacasismusic@gmail.com>
2024-09-20 14:20:40 +0200

    Prawn Added

---
+++ Assets/Scripts/Other/EnemyAnimScripts/PrawnAnims.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrawnAnims : MonoBehaviour
+{
+    public EnemyMovement enemyScript;
+    public Animator prawnAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyScript.distance > 20)
+        {
+            prawnAnimator.SetBool("InRange", false);
+            prawnAnimator.SetBool("AttackRange", false);
+        }
+        else
+        {
+            prawnAnimator.SetBool("InRange", true);
+        }
+
+        if (enemyScript.distance < 20 && enemyScript.distance > 10)
+        {
+            prawnAnimator.SetBool("AttackRange", false);
+        }
+        else
+        {
+            prawnAnimator.SetBool("AttackRange", true);
+            prawnAnimator.SetBool("InRange", true);
+        }
+    }
+}
