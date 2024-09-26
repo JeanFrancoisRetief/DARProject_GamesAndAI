ced9b62341fec9570b6e72b0cb739ff5b297f4b1
JeanFrancoisRetief <2458318@students.wits.ac.za>
2024-09-26 15:14:23 +0200

    improved random script

--- Assets/Scripts/Other/RandomSpawner.cs
+++ Assets/Scripts/Other/RandomSpawner.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class RandomSpawner : MonoBehaviour
 {
-    public GameObject Nat;
-    public GameObject Roach;
+    public GameObject Enemy;
+    
     public GameObject Arch;
     public int randomNum;
     // Start is called before the first frame update
     void Start()
     {
-        Nat.SetActive(false);
-        Roach.SetActive(false);
+        //Nat.SetActive(false);
+        //Roach.SetActive(false);
         Arch.SetActive(false);
         randomNum = Random.Range(1, 4);
         if (randomNum == 1)
         {
-            Nat.SetActive(true);
+            //Nat.SetActive(true);
+            GameObject spawnedNat = Instantiate(Enemy, transform);
+            spawnedNat.GetComponent<EnemyType>().currentEnemyType = EnemyType.Enemy_Type.Nat;
         }
         else if(randomNum == 2)
         {
-            Roach.SetActive(true);
+            GameObject spawnedRoach = Instantiate(Enemy, transform);
+            spawnedRoach.GetComponent<EnemyType>().currentEnemyType = EnemyType.Enemy_Type.Roach;
+            
         }
         else
         {
